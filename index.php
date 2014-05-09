ebddd595e6a20c36ac5a77fadf7ce8ab132e33cc
gustavo2012 <gustavo_3522@hotmail.com>
2014-05-08 22:11:07 -0500

    insertando metas

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title></title>
+	<meta charset="utf-8" />
+	<meta description="Proyecto de la primera generación del 2014 del curso de diseño web."> 
+	<!--descripcion de 160 caracteres, debe tener diferente descripcion cada pagina-->
+	<title>Puls2014: Comunidad en Perú</title>
 </head>
 <body>
 
